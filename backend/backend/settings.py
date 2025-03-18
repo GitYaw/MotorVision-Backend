8820aa0f2a9506cf7aed2c657fe45a3a39b8fb4f
Yaw Mireku <ymir8091@gmail.com>
2025-03-18 12:59:49 -0700

    Latest commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=2s4cw=uru$&cw%6jh_9i+e!j7u6$371*mcyu*vvufnpu)o^(w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
