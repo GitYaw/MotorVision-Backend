682a478f0462b4b43c709a61af6b82e833d00de0
Yaw Mireku <ymir8091@gmail.com>
2025-03-18 12:21:08 -0700

    Render service changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=2s4cw=uru$&cw%6jh_9i+e!j7u6$371*mcyu*vvufnpu)o^(w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
